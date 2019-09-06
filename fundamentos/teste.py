cac0fcad35e49d0bcb484f88a7ad9f18878cf656
Ricardo Trintin <paulorodriguestrintin@hotmail.com>
2019-09-06 11:40:06 -0300

    Tipos de dados

--- fundamentos/teste.py
+++ fundamentos/teste.py
@@ -1,4 +1,4 @@
-a = 3 
+a = 3
 b = 4
 
 print('valor de a: ', a)
---
+++ fundamentos/tipos_de_dados.py
@@ -0,0 +1,14 @@
+# Tipos basicos de dados
+
+print(True)
+print(False)
+print(1.2 + 1)
+print('Aqui eu posso falar qualquer texto')
+print("Aqui eu posso falar qualquer texto")
+print("Voce e " + 3 * "muito " + "legal")
+# print(3 + "3") => nao funciona pois existi ambiguidade
+list = ([1, 2, 3])
+dicionario = ({"nome": "Pedro", "Idade": 22})
+print(list)
+print(dicionario)
+print(None)
