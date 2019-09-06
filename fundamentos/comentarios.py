f80cc51ba3166c887c17c8443c388ad5dbd125a2
Ricardo Trintin <paulorodriguestrintin@hotmail.com>
2019-09-06 16:55:11 -0300

    Estrutura de comentarios

---
+++ fundamentos/comentarios.py
@@ -0,0 +1,24 @@
+# teste de comentario
+
+# comentario de 1 linha apenas (ctrl /)
+
+# sdkjnskjv
+# skdvksdvk
+# sdjvksdv
+
+# comentario de multiplas linhas (aspas tripla   """ )
+
+"""
+A idéia é calcular
+o quanto vai sobrar
+no final do mes!
+"""
+
+# Minhas variáveis
+salario = 3450.45
+despesas = 2456.2
+
+print(salario - despesas)
+
+# print("Fim")
+print("Fim de verdade")  # comentarios aqui tambem vale
