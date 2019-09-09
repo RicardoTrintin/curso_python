d3ed4c33af876c84053fa33a5a6cc55deed239e1
Ricardo Trintin <paulorodriguestrintin@hotmail.com>
2019-09-09 10:11:48 -0300

    adições nos operandos de membro e identidade

--- fundamentos/op_membro_identidade.py
+++ fundamentos/op_membro_identidade.py
@@ -11,3 +11,14 @@ z = 3
 print(x is y)
 print(y is z)
 print(z is not z)
+
+list_a = [1, 2, 3]
+list_b = list_a
+list_c = [1, 2, 3]
+
+# será igual a lista A pois estao compartilhando o mesmo espaço de memoria
+print(list_a is list_b)
+# não será igual a lista A pois nao ocupam o mesmo espaço de memoria
+print(list_b is list_c)
+
+print(list_a is not list_c)
