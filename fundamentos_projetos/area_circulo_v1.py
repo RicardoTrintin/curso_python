d172fb81d082d7372144fccd14c7d3a095a92e8d
Ricardo Trintin <paulorodriguestrintin@hotmail.com>
2019-09-22 20:55:28 -0300

    codigo circunferencia pegando dados do usuario e convertendo de String

---
+++ fundamentos_projetos/area_circulo_v1.py
@@ -0,0 +1,4 @@
+pi = 3.1415
+raio = 15.3
+result = pi * raio ** 2
+print("Area da circulo: ", result)
---
+++ fundamentos_projetos/area_circulo_v2.py
@@ -0,0 +1,4 @@
+pi = 3.1415
+raio = input("Informe o raio: ")
+result = pi * float(raio) ** 2
+print("Area da circulo: ", result)
