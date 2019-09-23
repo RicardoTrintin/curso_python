f4c732da6f3c6f40bee95b074dda41e02e87db5b
Ricardo Trintin <paulorodriguestrintin@hotmail.com>
2019-09-22 23:39:37 -0300

    Organizando melhor o codigo com estruturas de decisoes e criando uma class basica de cores

--- fundamentos_projetos/area_circulo_v2.py
+++ fundamentos_projetos/area_circulo_v2.py
@@ -1,10 +1,33 @@
+import sys
+import errno
+
+
+class TerminalColor:
+    ERRO = "\033[91m"
+    NORMAL = "\033[0m"
+
 
 def circulo(raio):
     pi = 3.1415
     result = pi * float(raio) ** 2
-    print("Area da circulo: ", result)
+    return result
+
+
+def help():
+    print("Area da circulo: ", area)
+    print("Area da circulo: {} <raio>".format(sys.argv[0]))
 
 
 if __name__ == "__main__":
-    raio = input("Informe o raio: ")
-    circulo(raio)
+    if len(sys.argv) < 2:
+        raio = input("Informe o raio: ")
+        area = circulo(raio)
+        help()
+    elif not sys.argv[1].isnumeric():
+        print(TerminalColor.ERRO + "Parametro nao suportado" +
+              TerminalColor.NORMAL)
+        sys.exit(errno.EINVAL)
+    else:
+        raio = sys.argv[1]
+        area = circulo(raio)
+        print("Area da circulo: ", area)
