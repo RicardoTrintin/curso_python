5f81a89f3cc31d63dd8052810677bda6f242b772
Ricardo Trintin <paulorodriguestrintin@hotmail.com>
2019-09-22 22:00:47 -0300

    Adicionando funçao ao projeto para nao executar direto assim que for improtada o modulo

--- fundamentos_projetos/area_circulo_v2.py
+++ fundamentos_projetos/area_circulo_v2.py
@@ -1,4 +1,10 @@
-pi = 3.1415
-raio = input("Informe o raio: ")
-result = pi * float(raio) ** 2
-print("Area da circulo: ", result)
+
+def circulo(raio):
+    pi = 3.1415
+    result = pi * float(raio) ** 2
+    print("Area da circulo: ", result)
+
+
+if __name__ == "__main__":
+    raio = input("Informe o raio: ")
+    circulo(raio)
